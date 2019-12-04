2801ecd3c6dbfeb3260795c76af95845db4920d5
Aaron Hauck <aaron.hauck@allstardirectories.com>
2019-12-03 16:20:57 -0800

    little update

--- s.bat
+++ s.bat
@@ -4,5 +4,4 @@ set _src=C:\src
 
 if %1==src      pushd %_src%
 if %1==""       pushd %_src%
-if %1==t        pushd %_src%\personal\tools
-if %1==p        pushd %_src%\personal\personal
+if %1==t        pushd %_src%\tools
